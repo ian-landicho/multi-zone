0e553d242376303c4dc90d33375443ea44d12806
Ian Landicho <alandicho@conservice.com>
2022-12-28 14:26:37 -0700

    Add detail view for state demo

--- app1/pages/index.tsx
+++ app1/pages/index.tsx
@@ -5,8 +5,43 @@ import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ data }: { data: [] }) {
+type Payee = {
+  depositTransferId: number;
+  paymentCountry: string;
+  amount: number;
+  payeeId: string;
+  payeeName: string;
+  payeeType: string;
+  numberOfBills: 14;
+  urgency: string;
+  depositTransferType: string;
+  depositTransferState: string;
+  paymentProcessor: string;
+  transactionCode: string;
+  fundingSource: string;
+  source: string;
+  paymentCorrelationId: string;
+  selected: boolean;
+};
+
+export default function Home({ data }: { data: Payee[] }) {
   const [count, setCount] = React.useState(0);
+  const [payees, setPayees] = React.useState(data);
+
+  function handleSelected(payeeId: string) {
+    setPayees(prev => {
+      const newPayees = prev.map(payee => {
+        if (payee.payeeId === payeeId) {
+          return {
+            ...payee,
+            selected: !payee.selected,
+          };
+        }
+        return payee;
+      });
+      return newPayees;
+    });
+  }
 
   return (
     <>
@@ -18,35 +53,51 @@ export default function Home({ data }: { data: [] }) {
       </Head>
       <main className={styles.main}>
         <div className={styles.grid}>
-          <a href="/" className={styles.card} rel="noopener noreferrer">
-            <h2 className={inter.className}>
-              <span>&lt;-</span> Portal
-            </h2>
+          <a
+            href="/"
+            className={styles.card}
+            rel="noopener noreferrer"
+            style={{ height: 50 }}
+          >
             <p className={inter.className}>Back to Portal</p>
           </a>
-          <div className={styles.description}>
-            <h1>Future DX</h1>
-            <br />
-            <br />
-            <h2>Count: {count}</h2>
-            <button
-              className={styles.button}
-              onClick={() => setCount(prev => prev + 1)}
-            >
-              INCREMENT
-            </button>
-            <h2>Names</h2>
-            <br />
-            <ul>
-              {data.map(item => {
-                return (
-                  <li key={item.depositTransferId}>
-                    {item.payeeId} - {item.payeeName}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          {payees.length > 0 ? (
+            <div className={styles.description}>
+              <h1>Future DX</h1>
+              <br />
+              <br />
+              <h2>Count: {count}</h2>
+              <button
+                className={styles.button}
+                onClick={() => setCount(prev => prev + 1)}
+              >
+                INCREMENT
+              </button>
+              <h2>Names</h2>
+              <br />
+              <div className={styles.payee}>
+                {payees.map(payee => {
+                  return (
+                    <div
+                      key={payee.depositTransferId}
+                      onClick={() => handleSelected(payee.payeeId)}
+                      style={{ padding: 10, fontSize: 16, cursor: "pointer" }}
+                    >
+                      {payee.payeeId} - {payee.payeeName}
+                      {payee.selected && (
+                        <p style={{ backgroundColor: "inherit" }}>
+                          Amount: ${payee.amount} Number of Bills:{" "}
+                          {payee.numberOfBills}
+                        </p>
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          ) : (
+            <div>Loading...</div>
+          )}
         </div>
       </main>
     </>
